dcabcdc561c958cefa0c96bfc40a5c88297a3267
johngrimes <john@grimes.id.au>
2025-12-01 13:54:30 +1000

    Remove basicSsl plugin

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
-import basicSsl from '@vitejs/plugin-basic-ssl';
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    svgr(),
-    basicSsl()
+    svgr()
   ],
   // Resolve absolute imports
   resolve: {
